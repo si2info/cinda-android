3bf35a5bf5d9f48bc3ddb5469cbfb2433841b12f
Jose Miguel Mingorance <jmmingorance@si2.info>
2015-09-07 09:28:10 +0200

    Layout de Campaña finalizado.

--- app/src/main/java/info/si2/iista/volunteernetworks/Campaign.java
+++ app/src/main/java/info/si2/iista/volunteernetworks/Campaign.java
@@ -3,6 +3,17 @@ package info.si2.iista.volunteernetworks;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import info.si2.iista.volunteernetworks.util.CircleTransform;
 
 /**
  * Developer: Jose Miguel Mingorance
@@ -16,11 +27,63 @@ public class Campaign extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.campaign);
 
+        // Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // CollapsingToolbar
         CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar);
         collapsingToolbarLayout.setTitle("A B C D E F G H I J K L M N Ñ O P Q R S T U V W X Y Z");
 
+        // Views
+        TextView objective = (TextView)findViewById(R.id.objective);
+        TextView geoArea = (TextView)findViewById(R.id.geoArea);
+        TextView dates = (TextView)findViewById(R.id.dates);
+        LinearLayout contributions = (LinearLayout)findViewById(R.id.contributions);
+
+        // TODO - Prueba para añadir contribuciones del usuario
+        for (int i=0; i<5; i++) {
+            View v = getLayoutInflater().inflate(R.layout.item_campaign_user, null);
+
+            // Views
+            ImageView imgUser = (ImageView)v.findViewById(R.id.imgUser);
+            TextView title = (TextView)v.findViewById(R.id.title);
+            TextView description = (TextView)v.findViewById(R.id.description);
+
+            Picasso.with(this)
+                    .load(R.drawable.test_logo_si2)
+                    .transform(new CircleTransform())
+                    .resize(150, 150)
+                    .into(imgUser);
+
+            contributions.addView(v);
+        }
+
     }
 
+    public void addContribution (View view) {
 
+        // TODO Intent a actividad "añadir contribución"
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_campaign, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
---
+++ app/src/main/java/info/si2/iista/volunteernetworks/util/ScrollAwareFABBehavior.java
@@ -0,0 +1,111 @@
+package info.si2.iista.volunteernetworks.util;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Interpolator;
+import android.view.animation.TranslateAnimation;
+
+/**
+ * Developer: Jose Miguel Mingorance
+ * Date: 7/9/15
+ * Project: Virde
+ */
+public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
+    private static final Interpolator INTERPOLATOR = new FastOutSlowInInterpolator();
+    private boolean mIsAnimatingIn = false;
+    private boolean mIsAnimatingOut = false;
+    private boolean isShowing = true;
+
+    @SuppressWarnings("UnusedParameters")
+    public ScrollAwareFABBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public boolean onStartNestedScroll(final CoordinatorLayout coordinatorLayout, final FloatingActionButton child,
+                                       final View directTargetChild, final View target, final int nestedScrollAxes) {
+        // Ensure we react to vertical scrolling
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
+                || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+    }
+
+    @Override
+    public void onNestedScroll(final CoordinatorLayout coordinatorLayout, final FloatingActionButton child,
+                               final View target, final int dxConsumed, final int dyConsumed,
+                               final int dxUnconsumed, final int dyUnconsumed) {
+
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        if (dyConsumed > 3 && !this.mIsAnimatingOut && isShowing) {
+            // User scrolled down and the FAB is currently visible -> hide the FAB
+            animateOut(child);
+        } else if (dyConsumed < -3 && !this.mIsAnimatingIn && !isShowing) {
+            // User scrolled up and the FAB is currently not visible -> show the FAB
+            animateIn(child);
+        }
+    }
+
+    // Same animation that FloatingActionButton.Behavior uses to hide the FAB when the AppBarLayout exits
+    private void animateOut(final FloatingActionButton button) {
+
+        TranslateAnimation anim = new TranslateAnimation(0, 0, Animation.RELATIVE_TO_SELF, 220.0f);
+
+        anim.setDuration(300L);
+        anim.setFillAfter(true);
+        anim.setFillEnabled(true);
+        anim.setInterpolator(INTERPOLATOR);
+
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            public void onAnimationStart(Animation animation) {
+                mIsAnimatingOut = true;
+            }
+
+            public void onAnimationEnd(Animation animation) {
+                mIsAnimatingOut = false;
+                isShowing = false;
+            }
+
+            @Override
+            public void onAnimationRepeat(final Animation animation) {
+            }
+        });
+
+        button.startAnimation(anim);
+
+    }
+
+    // Same animation that FloatingActionButton.Behavior uses to show the FAB when the AppBarLayout enters
+    private void animateIn(FloatingActionButton button) {
+
+        TranslateAnimation anim = new TranslateAnimation(0, 0, 220.0f, Animation.RELATIVE_TO_SELF);
+
+        anim.setDuration(300L);
+        anim.setFillAfter(true);
+        anim.setFillEnabled(true);
+        anim.setInterpolator(INTERPOLATOR);
+
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            public void onAnimationStart(Animation animation) {
+                mIsAnimatingIn = true;
+            }
+
+            public void onAnimationEnd(Animation animation) {
+                mIsAnimatingIn = false;
+                isShowing = true;
+            }
+
+            @Override
+            public void onAnimationRepeat(final Animation animation) {
+            }
+        });
+
+        button.startAnimation(anim);
+
+    }
+
+}
